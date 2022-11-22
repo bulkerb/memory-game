ac5219fa11b83e1884cc434940074212868a2abd
bulkerb <47120485+bulkerb@users.noreply.github.com>
2022-11-22 14:39:02 -0500

    testing curl directory

--- java/src/UrlConnectionReader.java
+++ java/src/UrlConnectionReader.java
@@ -1,16 +1,56 @@
-import java.net.*;  
-import java.io.*;  
+import java.net.*;
+import java.io.*;
 import java.util.*;
 
-public class UrlConnectionReader  
+public class UrlConnectionReader
 {  
   /**
  * @param args
  */
-public static void main(String[] args)  
-  {  
-    //https://emoji-api.com/emojis?access_key=f48301a44b0c8d06490563f08004880e0de02e51
-    String output  = getUrlContents("https://emoji-api.com/emojis/grinning-squinting-face?access_key=f48301a44b0c8d06490563f08004880e0de02e51");  
+public static void main(String[] args)
+  {
+    //references
+    String host = "https://emoji-api.com/";
+    String key = "?access_key=f48301a44b0c8d06490563f08004880e0de02e51";
+
+    //all api info https://emoji-api.com/emojis?access_key=f48301a44b0c8d06490563f08004880e0de02e51
+    String output = getUrlContents(host + "emojis" + key);
+    //System.out.println(output);
+
+    //categories: https://emoji-api.com/categories?access_key=f48301a44b0c8d06490563f08004880e0de02e51
+    output = getUrlContents(host + "categories" + key);
+    System.out.println(output);
+
+    //emoji in category
+    output = getUrlContents(host + "categories/smileys-emotion" + key);
+    List<String> cat = Arrays.asList(output.split("\\s*,\\s*"));
+    //System.out.println(output);
+
+    //get code for multiple emoji
+    Object unit = cat.get(3);
+    for(int i = 3; i<cat.size(); i+=6){
+      unit = cat.get(i);
+      System.out.println(unit);
+    }
+
+    //curl getting image
+    String command = "curl -X GET https://emojiapi.dev/api/v1/" + unit + "/512.png --output thisemoji.png";
+    ProcessBuilder im = new ProcessBuilder(command.split(" "));
+    im.directory(new File("memory-game/java/"));
+    Process process;
+    try {
+      process = im.start();
+      InputStream inputstream = process.getInputStream();
+      int exitCode=process.exitValue();
+      process.destroy();
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    
+
+    //one emoji
+    output = getUrlContents(host + "emojis/grinning-squinting-face" + key);
     System.out.println(output);
 
     //unicode
@@ -39,31 +79,30 @@ public static void main(String[] args)
     String two = "&#" + cc;
     uc.println(two);
     uc.close();
+
+    //get args info
+    if(args.length != 0){
+      output = getUrlContents(host + args[0] + key);
+      System.out.println("here");
+    }
   }  
-  
-  private static String getUrlContents(String theUrl)  
-  {  
-    StringBuilder content = new StringBuilder();  
-  // Use try and catch to avoid the exceptions  
-    try  
-    {  
-      URL url = new URL(theUrl); // creating a url object  
-      URLConnection urlConnection = url.openConnection(); // creating a urlconnection object  
-  
+
+  private static String getUrlContents(String theUrl)
+  {
+    StringBuilder content = new StringBuilder();
+  // Use try and catch to avoid the exceptions
+    try
+    {
+      URL url = new URL(theUrl); // creating a url object
+      URLConnection urlConnection = url.openConnection(); // creating a urlconnection object
       // wrapping the urlconnection in a bufferedreader  
-      BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));  
-      String line;  
-      // reading from the urlconnection using the bufferedreader  
-      while ((line = bufferedReader.readLine()) != null)  
-      {  
-        content.append(line + "\n");  
-      }  
-      bufferedReader.close();  
-    }  
-    catch(Exception e)  
-    {  
-      e.printStackTrace();  
-    }  
-    return content.toString();  
-  }  
-}  
+      BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+      String line;
+      // reading from the urlconnection using the bufferedreader
+      while ((line = bufferedReader.readLine()) != null){content.append(line + "\n");}
+      bufferedReader.close();
+    }
+    catch(Exception e){e.printStackTrace();}
+    return content.toString();
+  }
+}
