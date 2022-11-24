663b38c2955030aab0640a8bc20786e91054d3e3
bulkerb <47120485+bulkerb@users.noreply.github.com>
2022-11-24 03:39:04 -0500

    Delete App.java

--- java/src/App.java
+++
@@ -1,8 +0,0 @@
-import java.net.*;
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        URL url = new URL("https://emoji-api.com/emojis?access_key=f48301a44b0c8d06490563f08004880e0de02e51");
-        System.out.println("name: " + url.getFile());
-    }
-}
