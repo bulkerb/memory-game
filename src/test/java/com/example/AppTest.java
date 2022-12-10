c9146d3e422ba8afca291d91672e319fac1a25f1
bulkerb <47120485+bulkerb@users.noreply.github.com>
2022-12-10 07:58:58 -0500

    complete final

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -5,12 +5,12 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 /**
- * 
+ * Unit test for simple App.
  */
 public class AppTest 
 {
     /**
-     * 
+     * Rigorous Test :-)
      */
     @Test
     public void shouldAnswerWithTrue()
